733b2ffacd956706192dfaae5ddac3786dfe60a9
Kester Allen <kester@gmail.com>
2022-06-29 10:51:08 -0700

    MNT: Black formatting

--- surlyfritter/routes.py
+++ surlyfritter/routes.py
@@ -2,15 +2,13 @@
 Non-admin controllers for surlyfritter
 """
 
-#TODO print --> logging
-
 import datetime
-import dateutil.parser
 import io
 import os
 import random
 import re
 
+import dateutil.parser
 from flask import redirect, request, send_file, abort
 from google.cloud import storage
 from google.cloud.exceptions import NotFound
@@ -33,16 +31,18 @@ from surlyfritter.utils import (
 
 from . import app, client
 
+
 @app.errorhandler(404)
 def page_not_found(error):
     """Render a nice 404 page"""
     # Note 404 status is set explicitly
     return render_template("error_404.html", error=error), 404
 
+
 @app.route("/imgperm/<int:added_order>")
 @app.route("/imgp/<int:added_order>")
 @app.route("/ip/<int:added_order>")
-def image_perm(added_order:int):
+def image_perm(added_order: int):
     """
     Non-GCS (expensive!) image server.  Use this only if the GCS URL is
     unvailable.
@@ -51,19 +51,24 @@ def image_perm(added_order:int):
         try:
             picture = Picture.query(Picture.added_order == added_order).get()
             if picture is None:
-                print(f"""no Picture with .added_order of {added_order},
-                    returning most recent""")
+                print(
+                    f"""no Picture with .added_order of {added_order},
+                    returning most recent"""
+                )
                 picture = Picture.most_recent()
 
             storage_client = storage.Client()
             bucket = storage_client.bucket(GCS_BUCKET_NAME)
             blob = bucket.blob(picture.name)
             img = io.BytesIO(blob.download_as_bytes())
-        except NotFound as err: # blob not found
-            abort(404, f"blob for added_order={added_order} "
-                f"not found in image_perm; {err}")
+        except NotFound as err:  # blob not found
+            abort(
+                404,
+                f"blob for added_order={added_order} not found in image_perm; {err}",
+            )
 
         return send_file(img, mimetype="image/jpeg")
+
 
 @app.template_filter("shuffle")
 def filter_shuffle(seq):
@@ -75,6 +80,7 @@ def filter_shuffle(seq):
     except:
         return seq
 
+
 @app.template_filter("tag_fs_size")
 def filter_tagfontsize(tag):
     """A tag font size fitler for the template"""
@@ -83,6 +89,7 @@ def filter_tagfontsize(tag):
         return result
     except:
         return 6
+
 
 @app.route("/")
 @app.route("/d")
@@ -104,7 +111,7 @@ def filter_tagfontsize(tag):
 @app.route("/picture/<int:img_id>")
 @app.route("/p/<int:img_id>")
 @app.route("/<int:img_id>.jpg")
-def display(img_id:int=None):
+def display(img_id: int = None):
     """
     Display the picture that img_id == Picture.added_order for. If that doesn't
     exist, serve Picture.most_recent.
@@ -120,6 +127,7 @@ def display(img_id:int=None):
         html = render_template("display.html", pictures=pictures, tags=tags)
         return html
 
+
 @app.route("/display/highest")
 @app.route("/d/highest")
 def display_highest():
@@ -127,6 +135,7 @@ def display_highest():
     with client.context():
         picture = Picture.most_recent()
     return redirect(f"/p/{picture.added_order}")
+
 
 @app.route("/display/newest")
 @app.route("/d/mostrecent")
@@ -137,6 +146,7 @@ def display_newest():
     with client.context():
         picture = Picture.last_added()
     return redirect(f"/p/{picture.added_order}")
+
 
 @app.route("/display/oldest")
 @app.route("/d/oldest")
@@ -149,6 +159,7 @@ def display_oldest():
     with client.context():
         picture = Picture.least_recent()
     return redirect(f"/p/{picture.added_order}")
+
 
 @app.route("/random")
 def random_page():
@@ -165,10 +176,11 @@ def random_page():
 
         return redirect(f"/p/{index}")
 
+
 @app.route("/feed")
 @app.route("/feed/<int:max_num>")
 @app.route("/feeds/feed.xml")
-def feed(max_num:int=5):
+def feed(max_num: int = 5):
     """
     RSS feed for the most recent "max_num" pictures.
     """
@@ -176,16 +188,18 @@ def feed(max_num:int=5):
         pictures = Picture.query().order(-Picture.added_order).fetch(max_num)
         return render_template("feed.xml", pictures=pictures)
 
+
 @app.route("/tags/<tag_text>/all")
-def pictures_for_tags_all(tag_text:str):
+def pictures_for_tags_all(tag_text: str):
     """
     Display all pictures with the tag "tag_text"
     """
     return pictures_for_tags(tag_text=tag_text, max_num=None)
 
+
 @app.route("/tags/<tag_text>/<int:max_num>")
 @app.route("/tags/<tag_text>")
-def pictures_for_tags(tag_text:str, max_num:int=5):
+def pictures_for_tags(tag_text: str, max_num: int = 5):
     """
     Display N pictures with the tag "tag_text"
     """
@@ -194,40 +208,49 @@ def pictures_for_tags(tag_text:str, max_num:int=5):
         # large:
         total_count = Picture.with_tag_count(tag_text)
         pictures = Picture.with_tag(tag_text, num=max_num)
-        return render_template("display.html", pictures=pictures,
-            tag_text=tag_text, total_count=total_count)
+        return render_template(
+            "display.html",
+            pictures=pictures,
+            tag_text=tag_text,
+            total_count=total_count,
+        )
 
-def _kid_is(name:str, age_years:float):
+
+def _kid_is(name: str, age_years: float):
     """
     Return a redirect the display page for kid "name" at age "age_years"
     """
     with client.context():
-        added_order= Picture.kid_is_index(name, age_years)
+        added_order = Picture.kid_is_index(name, age_years)
         return redirect(f"/p/{added_order}")
+
 
 @app.route("/miri_is/<int:age_years>")
 @app.route("/miri_is/<float:age_years>")
-def miri_is(age_years:float):
+def miri_is(age_years: float):
     """
     Return a redirect the display page for miri at age "age_years"
     """
     return _kid_is("miri", age_years)
 
+
 @app.route("/julia_is/<int:age_years>")
 @app.route("/julia_is/<float:age_years>")
-def julia_is(age_years:float):
+def julia_is(age_years: float):
     """
     Return a redirect the display page for julia at age "age_years"
     """
     return _kid_is("julia", age_years)
 
+
 @app.route("/linus_is/<int:age_years>")
 @app.route("/linus_is/<float:age_years>")
-def linus_is(age_years:float):
+def linus_is(age_years: float):
     """
     Return a redirect the display page for linus at age "age_years"
     """
     return _kid_is("linus", age_years)
+
 
 @app.route("/same-age/<int:age_years>")
 @app.route("/same-age/<float:age_years>")
@@ -245,13 +268,14 @@ def same_age(age_years=None):
         linus = Picture.kid_is_index("linus", age_years)
         return redirect(f"/sbs/{miri}/{julia}/{linus}")
 
+
 @app.route("/side_by_side/<int:img_id1>/<int:img_id2>/<int:img_id3>/")
 @app.route("/sbs/<int:img_id1>/<int:img_id2>/<int:img_id3>/")
 @app.route("/display_multiple/<int:img_id1>/<int:img_id2>/<int:img_id3>/")
 @app.route("/multiple/<int:img_id1>/<int:img_id2>/<int:img_id3>/")
 @app.route("/dm/<int:img_id1>/<int:img_id2>/<int:img_id3>/")
 @app.route("/m/<int:img_id1>/<int:img_id2>/<int:img_id3>/")
-def side_by_side(img_id1:int, img_id2:int, img_id3:int):
+def side_by_side(img_id1: int, img_id2: int, img_id3: int):
     """Display the three images on one page"""
     with client.context():
         pictures = [
@@ -261,8 +285,9 @@ def side_by_side(img_id1:int, img_id2:int, img_id3:int):
         ]
         return render_template("display.html", pictures=pictures, side_by_side=True)
 
+
 @app.route("/mm/<img_ids_str>")
-def side_by_side_list(img_ids_str:str):
+def side_by_side_list(img_ids_str: str):
     """Display a list of images on one page"""
     img_ids = [int(i) for i in img_ids_str.split(",")]
     with client.context():
@@ -272,8 +297,9 @@ def side_by_side_list(img_ids_str:str):
             pictures.append(picture)
         return render_template("display.html", pictures=pictures, side_by_side=True)
 
+
 @app.route("/date/<date_str>")
-def display_date(date_str:str):
+def display_date(date_str: str):
     """
     Display the picture closest to "date_str"
     """
@@ -285,15 +311,17 @@ def display_date(date_str:str):
         picture = Picture.from_date(date)
         return redirect(f"/p/{picture.imgp_id}")
 
+
 @app.route("/timejump/<int:added_order>/<int(signed=True):years>")
 @app.route("/timejump/<int:added_order>/<float(signed=True):years>")
 @app.route("/tj/<int:added_order>/<int(signed=True):years>")
 @app.route("/tj/<int:added_order>/<float(signed=True):years>")
-def timejump(added_order:int, years:float):
+def timejump(added_order: int, years: float):
     """Display the page for "years" + the date of Picture.added_order"""
     with client.context():
         timejump_index = Picture.timejump_index(added_order, years)
         return redirect(f"/p/{timejump_index}")
+
 
 @app.route("/picture/add", methods=["GET", "POST"])
 def picture_add():
@@ -331,13 +359,14 @@ def picture_add():
             body=f"""
                 Added picture {picture.img_url} (/p/{picture.imgp_id})
                 {message}
-            """
+            """,
         )
         return redirect(f"/picture/add?message={message}")
     else:
         # GET: Render form with optional message
         message = request.args.get("message")
         return render_template("add.html", message=message)
+
 
 @app.route("/tag/add", methods=["POST"])
 def tag_add():
@@ -354,6 +383,7 @@ def tag_add():
 
     return redirect(f"/p/{added_order}")
 
+
 @app.route("/comment/add", methods=["POST"])
 def comment_add():
     """Add a comment to a picture"""
@@ -366,10 +396,11 @@ def comment_add():
 
     return redirect(f"/p/{added_order}")
 
+
 @app.route("/meta/<int:img_id>")
 @app.route("/p/meta/<int:img_id>")
 @app.route("/picture/meta/<int:img_id>")
-def meta(img_id:int):
+def meta(img_id: int):
     """Display a Picture's metadata, by its .added_order attribute"""
     with client.context():
         picture = Picture.query(Picture.added_order == img_id).get()
@@ -377,8 +408,9 @@ def meta(img_id:int):
             abort(404, f"No picture for ID {img_id}")
         return picture.meta
 
+
 @app.route("/exif/<int:img_id>")
-def exif(img_id:int):
+def exif(img_id: int):
     """Display a Picture's image exif metadata, by its .added_order attribute"""
     with client.context():
         picture = Picture.query(Picture.added_order == img_id).get()
@@ -386,8 +418,9 @@ def exif(img_id:int):
             abort(404, f"No picture to get exif from for ID {img_id}")
         return get_exif_data_from_url(picture.img_url)
 
+
 @app.route("/hash/<int:img_id>")
-def hash(img_id:int):
+def imghash(img_id: int):
     """Display a Picture's MD5 hash, by its .added_order attribute"""
     with client.context():
         picture = Picture.query(Picture.added_order == img_id).get()
@@ -395,6 +428,7 @@ def hash(img_id:int):
             abort(404, f"No picture to get exif from for ID {img_id}")
         hash_value = get_hash_from_url(picture.img_url)
     return hash_value
+
 
 @app.route("/gone_with_the_wind", methods=["GET", "POST"])
 @app.route("/gone-with-the-wind", methods=["GET", "POST"])
@@ -407,7 +441,8 @@ def gone_with_the_wind():
 
     def _img_from_url(url, newsize=None):
         resp = requests.get(url, stream=True)
-        resp.raw.decode_content = True # Content-Encoding ## WTF does this mean, three-weeks-ago-Me?
+        # https://stackoverflow.com/questions/16923898/how-to-get-the-raw-content-of-a-response-in-requests-with-python
+        resp.raw.decode_content = True  # Content-Encoding
         img = Image.open(resp.raw)
         if newsize is not None:
             img = img.resize(newsize)
@@ -420,8 +455,8 @@ def gone_with_the_wind():
         return send_file(buf, mimetype="image/jpeg")
 
     if request.method == "POST":
-        w0 = 256;
-        h0 = 256;
+        w0 = 256
+        h0 = 256
         resize = (w0, h0)
 
         background = _img_from_url(burning_planation_url, (2048, 2048))
@@ -429,19 +464,21 @@ def gone_with_the_wind():
         for i, picture in enumerate(pictures):
             offset = (64 + i * w0, 64 + i * h0)
 
-            img = Image.open(picture).resize(resize).rotate(15*i)
+            img = Image.open(picture).resize(resize).rotate(15 * i)
             background.paste(img, offset)
         return _send_img_bytes_io(background)
     else:
         return render_template("gone_with_the_wind.html")
 
-def _kid_is(name:str, age_years:float):
+
+def _kid_is(name: str, age_years: float):
     """
     Return a redirect the display page for kid "name" at age "age_years"
     """
     with client.context():
-        added_order= Picture.kid_is_index(name, age_years)
+        added_order = Picture.kid_is_index(name, age_years)
         return redirect(f"/p/{added_order}")
+
 
 @app.route("/jazz_show_name")
 @app.route("/jazz_oclock")
@@ -452,7 +489,8 @@ def jazz_show_name():
     Return the name of the current jazz show
     """
     now = datetime.datetime.now().strftime("%H:%M O'clock")
-    return(f"The {now} Jazz Show")
+    return f"The {now} Jazz Show"
+
 
 def _usgs_url(layer, box_str, width, height, map_):
     url = (
@@ -467,8 +505,9 @@ def _usgs_url(layer, box_str, width, height, map_):
     )
     return url
 
+
 @app.route("/venus/<box_str>/<int:width>/<int:height>")
-def usgs_venus(box_str:float, width:int, height:int):
+def usgs_venus(box_str: float, width: int, height: int):
     """
     Redirect to USGS Venus server. Putting a redirect here since Twitter is
     trimming these very long URLs from https://twitter.com/magellanvenus.
@@ -478,8 +517,9 @@ def usgs_venus(box_str:float, width:int, height:int):
     map_ = "/maps/venus/venus_simp_cyl.map"
     return redirect(_usgs_url(layer, box_str, width, height, map_))
 
+
 @app.route("/mercury/<box_str>/<int:width>/<int:height>")
-def usgs_mercury(box_str:float, width:int, height:int):
+def usgs_mercury(box_str: float, width: int, height: int):
     """
     Redirect to USGS Mercury server. Putting a redirect here since Twitter is
     trimming these very long URLs from https://twitter.com/bitsofmercury.
